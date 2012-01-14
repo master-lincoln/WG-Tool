103276efe881759fbd7c746ebd9c68ce1a337bec
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-01-14 03:34:58 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WGTool::Application.config.secret_token = '27defe11caf53a35d149cccfd5f644e8df213f503061c5f1b5de1cbe54d11ff84f08305a590c8335f7cd1c13bfada81aec06a43c51852f516bd6fac5900f20f7'
