3ccd6442fd1cce2788313835c42f7bbd8e145c92
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-03-20 22:53:13 +0100

    sort invoices

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,7 +2,7 @@ class InvoicesController < ApplicationController
   # GET /invoices
   # GET /invoices.json
   def index
-    @invoices = Invoice.all.reverse
+    @invoices = Invoice.order('created_at DESC').all
 
     respond_to do |format|
       format.html # index.html.erb
