9b2613ccbb19c951d70f6dd8e05068f46aca853c
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-06-19 20:29:47 +0200

    accept commas as decimal dividers (fixes #8)

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ActiveRecord::Base
 	belongs_to :creator, :class_name => "User"
 	has_many :duties
 	has_many :users, :through => :duties
-	
+
 	def individual_sum
 		price / duties.count
 	end
