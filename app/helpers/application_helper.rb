abe42193bb21a67bbfc45de4004102619562edc5
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2013-09-04 15:22:36 +0200

    even prettier output for changes in notification mail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,24 @@ module ApplicationHelper
   def euro(price)
   	raw ( "%.2f &euro;" % price )
   end
+
+  def format_diff_as_table(diff)
+  	columns = "".html_safe
+  	diff.each do |key,value|
+  		changes = value.map { |change| content_tag(:td, change) }.join
+  		columns += "<tr><td>".html_safe
+  		columns << key
+  		columns << "</td>#{changes}</tr>".html_safe
+  	end
+
+  	"<table>
+  		<thead>
+  			<tr><th>Field</th><th>Before</th><th>After</th></tr>
+  		</thead>
+  		<tbody>
+  			#{columns}
+  		</tbody>
+  	</table>".html_safe
+  end
+
 end
